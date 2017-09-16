5a3f71d2bfe4b310944d5587ecbdb738ea35db2b
Chih-Hao <zhihaozhang@me.com>
2017-09-16 21:48:54 +0800

    Initial Commit

---
+++ mouseSync/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mouseSync
+//
+//  Created by Chih-Hao on 2017/9/16.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mouseSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mouseSync
+//
+//  Created by Chih-Hao on 2017/9/16.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
